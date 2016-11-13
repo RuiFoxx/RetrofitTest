01503451368ecb9b0142c1acab0c6869199b839a
RuiFoxx <fox.uchiha95@gmail.com>
2016-11-13 13:00:22 +0700

    Retrofit Test

---
+++ app/src/main/java/com/ruifoxx/retrofittest/MainActivity.java
@@ -0,0 +1,69 @@
+package com.ruifoxx.retrofittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText text;
+    private TextView translated;
+    private Button translateBtn;
+    private final String URL = "https://translate.yandex.ru";
+    private final String KEY = "trnsl.1.1.20161113T050255Z.5ee1ca7d5dbbd970.b82f8c250d226be570b710628b3c273006fc862f";
+
+    private Gson gson = new GsonBuilder().create();
+    private Retrofit retrofit = new Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .baseUrl(URL)
+            .build();
+
+    private Link intf = retrofit.create(Link.class);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        text = (EditText) findViewById(R.id.text);
+        translated = (TextView) findViewById(R.id.translated);
+        translateBtn = (Button) findViewById(R.id.translate);
+
+        translateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, String> mapJson = new HashMap<String, String>();
+                mapJson.put("key", KEY);
+                mapJson.put("text", text.toString());
+                mapJson.put("lang", "en-ru");
+                Call<Object> call = intf.translate(mapJson);
+                try {
+                    Response<Object> response = call.execute();
+
+                    Map<String, String> map = gson.fromJson(response.body().toString(), Map.class);
+
+                    for(Map.Entry e : map.entrySet()) {
+                        if(e.getKey().equals("text"))
+                            translated.setText(e.getValue().toString());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
